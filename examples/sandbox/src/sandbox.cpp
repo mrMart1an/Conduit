b632da00bd15db6e392edbaedcac7351bad37df6
Andrea Greco <andreagreco2004@outlook.com>
2024-04-06 19:08:14 +0200

    remove test code

--- examples/sandbox/src/sandbox.cpp
+++ examples/sandbox/src/sandbox.cpp
@@ -2,55 +2,16 @@
 
 #include "conduit/application.h"
 
-#include "conduit/events/eventWriter.h"
-#include "conduit/events/events.h"
 #include "conduit/logging.h"
 
 using namespace cndt;
 using namespace cndt::log::app;
 
-void keyCall(const KeyEvent* event) {
-    info("callback key: {}", event->key_code);
-}
-
-void winCall(const WindowResize* event) {
-    info("callback resize: {}, {}", event->width, event->height);
-}
-
 // Declare the application class
 class Sandbox : public cndt::Application {
 public:
     void startup() override {
-        EventWriter writer = m_event_bus.getEventWriter();
         
-        m_event_bus.addCallback<WindowResize>(winCall);
-        m_event_bus.addCallback<KeyEvent>(keyCall);
-        
-        EventReader reader = m_event_bus.getEventReader<KeyEvent>();
-        
-        WindowResize resize;
-        KeyEvent event;
-        
-        event.key_code = 12;
-        writer.send(event);
-        event.key_code = 62;
-        writer.send(event);
-
-        m_event_bus.update();
-        
-        for (auto& event : reader) {
-            info("reader key event: {}", event.key_code);
-        }
-
-        resize.height = 20; resize.width = 32;
-        writer.send(resize);
-        resize.height = 2320; resize.width = 334322;
-        writer.send(resize);
-        resize.height = 42; resize.width = 69;
-        writer.send(resize);
-
-        m_event_bus.update();
-
     }
     
     void update(f32 delta_time) override {
