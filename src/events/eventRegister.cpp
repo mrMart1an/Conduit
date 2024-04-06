711b7abdb65a654252add2967ab12b475cc06729
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2024-04-06 15:28:03 +0200

    callback register thread safe

--- src/events/eventRegister.cpp
+++ src/events/eventRegister.cpp
@@ -8,13 +8,15 @@ constexpr usize default_buffers_size = 30;
 
 EventRegister::EventRegister()
 { 
-    std::unique_lock<std::shared_mutex> lock(m_mutex);
+    std::lock_guard<std::mutex> lock(m_mutex);
     m_event_buffers.reserve(default_buffers_size);
 }
 
 // Swap and clear the event buffers
 void EventRegister::update() 
 {
+    std::lock_guard<std::mutex> lock(m_mutex);
+    
     for (auto& buffer : m_event_buffers) {
         buffer->update();
     }
