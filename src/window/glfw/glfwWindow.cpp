673f465479e6e9614dd6b30a294699ce24adde3f
Andrea Greco <andreagreco2004@outlook.com>
2024-04-08 19:15:25 +0200

    glfw window

---
+++ src/window/glfw/glfwWindow.cpp
@@ -0,0 +1,254 @@
+#include "conduit/logging.h"
+
+#include "conduit/events/eventWriter.h"
+#include "conduit/window/window.h"
+
+#include "window/glfw/glfwWindow.h"
+
+#include <GLFW/glfw3.h>
+
+namespace cndt::glfw {
+
+// Glfw window constructor
+GlfwWindow::GlfwWindow(EventWriter event_writer) : 
+    m_event_writer(event_writer), 
+    m_fullscreen(false),
+    m_current_data(),
+    m_old_data(),
+    m_init_status(Status::Uninitialized)
+{ }
+
+// Glfw window destructor
+GlfwWindow::~GlfwWindow() 
+{
+    if (m_init_status != Status::Uninitialized) {
+        log::core::warn(
+            "GlfwWindow::~GlfwWindow -> destructor called before shutdown"
+        );
+        
+        shutdown();
+    }
+}
+
+// Glfw window initialization function
+void GlfwWindow::initialize(Config config)
+{
+    if (!glfwInit()) {
+        log::core::error(
+            "GlfwWindow::GlfwWindow -> glfw initialization failed"
+        );
+        
+        throw WindowInitError("glfw initialization failed");
+    }
+    
+    // Set the status to glfw initialized
+    m_init_status = Status::GlfwInitialized;
+    
+    // Set the error message callback
+    glfwSetErrorCallback(callback_error);
+    
+    // Window creation hints
+    glfwWindowHint(GLFW_RESIZABLE, config.resizable ? GLFW_TRUE : GLFW_FALSE);
+    glfwWindowHint(GLFW_FLOATING, config.floating ? GLFW_TRUE : GLFW_FALSE);   
+
+    // Disable OpenGL context creation
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    
+    // Create the glfw window handler
+    m_glfw_window = glfwCreateWindow(
+        config.width, 
+        config.height,
+        config.title.c_str(),
+        NULL, NULL
+    );
+
+    // Check if the window was create successfully 
+    if (m_glfw_window == nullptr) {
+        log::core::error(
+            "GlfwWindow::GlfwWindow -> glfw window creation failed"
+        );
+        
+        // Terminate the glfw session and throw an init exception
+        glfwTerminate();
+        throw WindowInitError("glfw window creation failed");
+    }
+    
+    // Store the current data
+    glfwGetWindowPos(
+        m_glfw_window, 
+        &m_current_data.x_pos, 
+        &m_current_data.y_pos 
+    );
+    
+    glfwGetWindowSize(
+        m_glfw_window, 
+        &m_current_data.window_width, 
+        &m_current_data.window_height 
+    );
+    glfwGetFramebufferSize(
+        m_glfw_window, 
+        &m_current_data.buffer_width, 
+        &m_current_data.buffer_height 
+    );
+    
+    // Set fullscreen if requested
+    setFullscreen(config.fullscreen);
+
+    // Set the callback data pointer to the current window object
+    glfwSetWindowUserPointer(m_glfw_window, this);
+    
+    // Set input event callbacks
+    glfwSetKeyCallback(m_glfw_window, callback_key_event);
+    glfwSetMouseButtonCallback(m_glfw_window, callback_mouse_button_event);
+    glfwSetCursorPosCallback(m_glfw_window, callback_cursor_pos_event);
+    glfwSetScrollCallback(m_glfw_window, callback_scroll_event);
+
+    // Set window event callbacks
+    glfwSetWindowSizeCallback(m_glfw_window, callback_window_resize_event);
+    glfwSetWindowPosCallback(m_glfw_window, callback_window_move_event);
+    glfwSetWindowCloseCallback(m_glfw_window, callback_window_close_event);
+    glfwSetCursorEnterCallback(m_glfw_window, callback_cursor_entered_event);
+    glfwSetFramebufferSizeCallback(
+        m_glfw_window, callback_buffer_resize_event
+    );
+
+    // Set the status to initialized
+    m_init_status = Status::Initialized;
+}
+
+// Glfw window shutdown function 
+void GlfwWindow::shutdown() 
+{
+    if (m_init_status == Status::Initialized) {
+        glfwDestroyWindow(m_glfw_window);
+        glfwTerminate();
+    } else if (m_init_status == Status::GlfwInitialized) {
+        glfwTerminate();
+    }
+
+    m_init_status = Status::Uninitialized;
+}
+
+// Obtain the current window data 
+Window::Data GlfwWindow::getWindowData()
+{
+    return m_current_data;
+}
+
+// Toggle the full screen mode of the window
+void GlfwWindow::toggleFullscreen() 
+{
+    setFullscreen(!m_fullscreen);
+}
+
+// Set the full screen mode of the window
+void GlfwWindow::setFullscreen(bool fullscreen) 
+{
+    // If the requested mode is already enable return immediately
+    if (m_fullscreen == fullscreen)
+        return;
+    
+    // Set full screen
+    if (fullscreen) {
+        // Store the old windowed mode data
+        m_old_data = m_current_data;
+
+        // Get the monitor and the video mode
+        GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+        const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+
+        glfwSetWindowMonitor(
+            m_glfw_window,
+            monitor,
+            0, 0,
+            mode->width, mode->height,
+            mode->refreshRate
+        );
+
+    } else {
+        // Restore the window to the pre-fullscreen state
+        glfwSetWindowMonitor(
+            m_glfw_window,
+            NULL,
+            m_old_data.x_pos, m_old_data.y_pos,
+            m_old_data.window_width, m_old_data.window_height,
+            GLFW_DONT_CARE
+        );
+    }
+
+    // Store the current data
+    glfwGetWindowPos(
+        m_glfw_window, 
+        &m_current_data.x_pos, 
+        &m_current_data.y_pos 
+    );
+    
+    glfwGetWindowSize(
+        m_glfw_window, 
+        &m_current_data.window_width, 
+        &m_current_data.window_height 
+    );
+    glfwGetFramebufferSize(
+        m_glfw_window, 
+        &m_current_data.buffer_width, 
+        &m_current_data.buffer_height 
+    );
+ 
+    // Store the new full screen setting
+    m_fullscreen = fullscreen;
+}
+
+// Change the window size in windowed mode
+void GlfwWindow::setSize(i32 width, i32 height) 
+{
+    if (m_fullscreen)
+        return;
+    
+    glfwSetWindowSize(m_glfw_window, width, height);
+    
+    // Store the current data
+    glfwGetWindowPos(
+        m_glfw_window, 
+        &m_current_data.x_pos, 
+        &m_current_data.y_pos 
+    );
+    
+    glfwGetWindowSize(
+        m_glfw_window, 
+        &m_current_data.window_width, 
+        &m_current_data.window_height 
+    );
+    glfwGetFramebufferSize(
+        m_glfw_window, 
+        &m_current_data.buffer_width, 
+        &m_current_data.buffer_height 
+    );
+}
+
+// Change the window title
+void GlfwWindow::setTitle(const char* title)
+{
+    glfwSetWindowTitle(m_glfw_window, title);
+}
+
+// Capture the cursor and enable raw input if available
+void GlfwWindow::captureCursor() 
+{
+    glfwSetInputMode(m_glfw_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetInputMode(m_glfw_window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+}
+
+// Release the cursor and disable raw input if available
+void GlfwWindow::releaseCursor() 
+{
+    glfwSetInputMode(m_glfw_window, GLFW_RAW_MOUSE_MOTION, GLFW_FALSE);
+    glfwSetInputMode(m_glfw_window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+}
+
+// Pool the window event and send them to the event bus
+void GlfwWindow::poolEvents() 
+{
+    glfwPollEvents();
+}
+
+} // namespace cndt::glfw
---
+++ src/window/glfw/glfwWindow.h
@@ -0,0 +1,119 @@
+#ifndef CNDT_GLFW_WINDOW_H
+#define CNDT_GLFW_WINDOW_H
+
+#include "conduit/events/eventWriter.h"
+#include "conduit/window/window.h"
+
+#include <GLFW/glfw3.h>
+
+namespace cndt::glfw {
+
+// Glfw window class
+class GlfwWindow : public Window {
+private:
+    // Window current initialization status enum
+    enum class Status {
+        Uninitialized = 0,  
+        GlfwInitialized,
+        Initialized,
+    };
+
+public:
+    GlfwWindow(EventWriter event_writer);
+    ~GlfwWindow() override;
+
+    // Obtain the current window data 
+    Data getWindowData() override;
+
+    // Toggle the full screen mode of the window
+    void toggleFullscreen() override;
+    // Set the full screen mode of the window
+    void setFullscreen(bool fullscreen) override;
+    
+    // Change window title
+    void setTitle(const char* title) override;
+
+    // Change the window size in windowed mode
+    void setSize(i32 width, i32 height) override;
+    
+    // Capture the cursor and enable raw input if available
+    void captureCursor() override;
+    // Release the cursor and disable raw input if available
+    void releaseCursor() override;
+    
+private:
+    // Initialize the glfw window
+    void initialize(Config config) override;
+
+    // Shutdown the glfw window
+    void shutdown() override;
+    
+    // Pool the window event and send them to the event bus
+    void poolEvents() override;
+    
+private:
+    // Glfw window handle
+    GLFWwindow *m_glfw_window;
+
+    // Used to send window event to the application bus
+    EventWriter m_event_writer;
+
+    // Current fullscreen status
+    bool m_fullscreen;
+
+    // Current window position and size data
+    Data m_current_data;
+    // Old window position and size data
+    // used to restore the window size and position when leaving fullscreen
+    Data m_old_data;
+
+    // Initialization status
+    Status m_init_status;
+
+// Glfw callbacks declaration
+private:
+    // Callback functions for mouse button events 
+    static void callback_mouse_button_event(
+        GLFWwindow* window, int button, int action, int mods
+    );
+    // Callback functions for mouse position events 
+    static void callback_cursor_pos_event(
+        GLFWwindow* window, double xpos, double ypos
+    );
+    // Callback functions for mouse scrolling events 
+    static void callback_scroll_event(
+        GLFWwindow* window, double xoffset, double yoffset
+    );
+    // Callback functions for keyboard events 
+    static void callback_key_event(
+        GLFWwindow* window, int key, int scancode, int action, int mods
+    );
+    
+    // Callback functions for window cursor enter and exit events
+    static void callback_cursor_entered_event(
+        GLFWwindow* window, int entered
+    );
+    // Callback functions for window move events
+    static void callback_window_move_event(
+        GLFWwindow* window, int xpos, int ypos
+    );
+    // Callback functions for window resize events
+    static void callback_window_resize_event(
+        GLFWwindow* window, int width, int height
+    );
+    // Callback functions for window resize events
+    static void callback_buffer_resize_event(
+        GLFWwindow* window, int width, int height
+    );
+    
+    // Callback functions for window close events
+    static void callback_window_close_event(GLFWwindow* window);
+    
+    // Glfw error callback function 
+    // Print the error with the internal logger
+    static void callback_error(int error_code, const char* error_msg);
+};
+
+} // namespace cndt::glfw
+
+#endif
