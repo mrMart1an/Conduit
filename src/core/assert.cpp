6efd15354afd53655cf1493effe89867f0bf21fd
Andrea Greco <andreagreco2004@outlook.com>
2024-04-10 12:30:47 +0200

    assert

---
+++ src/core/assert.cpp
@@ -0,0 +1,25 @@
+#include "conduit/defines.h"
+#include "conduit/logging.h"
+
+#include "conduit/assert.h"
+
+namespace cndt::internal {
+
+// Conduit assert function declaration
+void assert(
+    bool cond,
+    std::string_view cond_str,
+    std::string_view file,
+    i32 line
+) {
+    if (!cond) {
+        log::core::fatal(
+            "Assert fail: \"{}\"; file: {}; line: {}",
+            cond_str, file, line
+        );
+
+        CNDT_ABORT();
+    }
+}
+
+} // namespace cndt::core
