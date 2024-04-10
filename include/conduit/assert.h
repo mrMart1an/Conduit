11b314235ba5cd6532692e2f6bbd016d7796a1f9
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2024-04-10 12:30:47 +0200

    assert

--- include/conduit/assert.h
+++ include/conduit/assert.h
@@ -1,6 +1,28 @@
 #ifndef CNDT_ASSERT_H
 #define CNDT_ASSERT_H
 
+#include "conduit/defines.h"
 
+#include <string_view>
+
+// Conduit assert macro
+#ifndef NDEBUG
+    #define CNDT_ASSERT(cond)   \
+        cndt::internal::assert(cond, #cond, __FILE__, __LINE__);
+#else
+    #define CNDT_ASSERT(cond, msg)
+#endif
+
+namespace cndt::internal {
+
+// Conduit assert function declaration
+void assert(
+    bool cond,
+    std::string_view cond_str,
+    std::string_view file,
+    i32 line
+);
+
+} // namespace cndt::core
 
 #endif
