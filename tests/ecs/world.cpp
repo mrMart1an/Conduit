8e5c885891b907f07fc295c9c160b6dcff9ee16d
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2024-06-17 17:31:54 +0200

    query tests

--- tests/ecs/world.cpp
+++ tests/ecs/world.cpp
@@ -25,66 +25,276 @@ struct CompSecond {
     
     int r;  
 };
+struct CompThird {
+    CompThird() : a(0) {}; 
+    CompThird(int a) : a(a) {}; 
+    
+    int a;
+};
 
-TEST(query_test, world_test) {
+TEST(query_read_test, world_test) {
     World world;
 
     std::vector<Entity> entities;
     
-    for (int i = 0; i < 15; i++) {
+    // Create entities
+    for (int i = 0; i < 20; i++) {
         Entity e = world.newEntity();
         entities.push_back(e);
     }
 
     // Create components
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 15; i++) {
         Entity e = entities.at(i);
         world.attachComponent<CompFirst>(e, 10 + i);
     }
-    for (int i = 5; i < 15; i++) {
+    for (int i = 5; i < 20; i++) {
         Entity e = entities.at(i);
         world.attachComponent<CompSecond>(e, 20 + i);
+    }
+    for (int i = 0; i < 10; i++) {
+        Entity e = entities.at(i*2);
+        world.attachComponent<CompThird>(e, 30 + i*2);
+    }
+
+    // Single query test
+    {
+        auto query_first = world.createQuery<CompFirst>();
+
+        ASSERT_EQ(15, query_first.size());
+        for (auto element : query_first) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+        }
+        
+        auto query_second = world.createQuery<CompSecond>();
+
+        ASSERT_EQ(15, query_second.size());
+        for (auto element : query_second) {
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+        }
+
+        auto query_third = world.createQuery<CompThird>();
+
+        ASSERT_EQ(10, query_third.size());
+        for (auto element : query_third) {
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }   
+    
+    // Double query test
+    {
+        auto query = world.createQuery<CompSecond, CompFirst>();
+    
+        ASSERT_EQ(10, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+        }
+    }
+    {
+        auto query = world.createQuery<CompFirst, CompSecond>();
+
+        ASSERT_EQ(10, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+        }
+    }
+    {
+        auto query = world.createQuery<CompFirst, CompThird>();
+
+        ASSERT_EQ(8, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }
+    {
+        auto query = world.createQuery<CompThird, CompSecond>();
+
+        ASSERT_EQ(7, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }
+
+    // Triple query test
+    {
+        auto query = world.createQuery<CompThird, CompSecond, CompFirst>();
+    
+        ASSERT_EQ(5, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }
+    {
+        auto query = world.createQuery<CompFirst, CompSecond, CompThird>();
+    
+        ASSERT_EQ(5, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }
+    {
+        auto query = world.createQuery<CompFirst, CompThird, CompSecond>();
+    
+        ASSERT_EQ(5, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }
+    {
+        auto query = world.createQuery<CompSecond, CompThird, CompFirst>();
+    
+        ASSERT_EQ(5, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+            ASSERT_EQ(element.entity().id(), element.get<CompThird>().a - 30);
+        }
+    }
+}
+
+TEST(query_write_test, world_test) {
+    World world;
+
+    std::vector<Entity> entities;
+    
+    // Create entities
+    for (int i = 0; i < 20; i++) {
+        Entity e = world.newEntity();
+        entities.push_back(e);
+    }
+
+    // Create components
+    for (int i = 0; i < 15; i++) {
+        Entity e = entities.at(i);
+        world.attachComponent<CompFirst>(e, 1);
+    }
+    for (int i = 5; i < 20; i++) {
+        Entity e = entities.at(i);
+        world.attachComponent<CompSecond>(e, 2);
+    }
+
+    // Read and write to the components
+    {
+        auto query = world.createQuery<CompFirst, CompSecond>();
+
+        ASSERT_EQ(10, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(1, element.get<CompFirst>().x);
+            ASSERT_EQ(2, element.get<CompSecond>().r);
+
+            element.get<CompFirst>().x = element.entity().id() + 10;
+            element.get<CompSecond>().r = element.entity().id() + 20;
+            
+            ASSERT_EQ(element.entity().id() + 10, element.get<CompFirst>().x);
+            ASSERT_EQ(element.entity().id() + 20, element.get<CompSecond>().r);
+        }
+    }
+    
+    // Create a different query and test again
+    {
+        auto query = world.createQuery<CompSecond, CompFirst>();
+
+        ASSERT_EQ(10, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(element.entity().id() + 10, element.get<CompFirst>().x);
+            ASSERT_EQ(element.entity().id() + 20, element.get<CompSecond>().r);
+        }
+    }
+} 
+
+
+TEST(query_update_test, world_test) {
+    World world;
+
+    std::vector<Entity> entities;
+    
+    // Create entities
+    for (int i = 0; i < 20; i++) {
+        Entity e = world.newEntity();
+        entities.push_back(e);
+    }
+    
+    // First update
+    for (int i = 0; i < 5; i++) {
+        Entity e = entities.at(i);
+        world.attachComponent<CompFirst>(e, 1);
+    }
+    for (int i = 0; i < 10; i++) {
+        Entity e = entities.at(i);
+        world.attachComponent<CompSecond>(e, 2);
+    }
+
+    {
+        auto query = world.createQuery<CompSecond, CompFirst>();
+
+        ASSERT_EQ(5, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(1, element.get<CompFirst>().x);
+            ASSERT_EQ(2, element.get<CompSecond>().r);
+        }
+    }
+    
+    // Second update
+    for (int i = 5; i < 15; i++) {
+        Entity e = entities.at(i);
+        world.attachComponent<CompFirst>(e, 1);
     }
     
     {
         auto query = world.createQuery<CompSecond, CompFirst>();
-    
-        ASSERT_EQ(5, query.size());
-        for (auto element : query) {
-            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
-            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
-        }
-    }
-    
-    {
-        auto query = world.createQuery<CompFirst, CompSecond>();
-
-        ASSERT_EQ(5, query.size());
-        for (auto element : query) {
-            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
-            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
-        }
-    }
-    
-    {
-        auto query = world.createQuery<CompFirst>();
 
         ASSERT_EQ(10, query.size());
         for (auto element : query) {
-            ASSERT_EQ(element.entity().id(), element.get<CompFirst>().x - 10);
+            ASSERT_EQ(1, element.get<CompFirst>().x);
+            ASSERT_EQ(2, element.get<CompSecond>().r);
         }
+    }
+
+    // Third update
+    for (int i = 10; i < 20; i++) {
+        Entity e = entities.at(i);
+        world.attachComponent<CompSecond>(e, 2);
     }
     
     {
-        auto query = world.createQuery<CompSecond>();
+        auto query = world.createQuery<CompSecond, CompFirst>();
 
-        ASSERT_EQ(10, query.size());
+        ASSERT_EQ(15, query.size());
         for (auto element : query) {
-            ASSERT_EQ(element.entity().id(), element.get<CompSecond>().r - 20);
+            ASSERT_EQ(1, element.get<CompFirst>().x);
+            ASSERT_EQ(2, element.get<CompSecond>().r);
         }
     }
 
+    // Fourth update
+    for (int i = 0; i < 5; i++) {
+        Entity e = entities.at(i);
+        world.detachComponent<CompFirst>(e);
+    }
+    for (int i = 5; i < 10; i++) {
+        Entity e = entities.at(i);
+        world.detachComponent<CompSecond>(e);
+    }
+    
+    {
+        auto query = world.createQuery<CompSecond, CompFirst>();
 
+        ASSERT_EQ(5, query.size());
+        for (auto element : query) {
+            ASSERT_EQ(1, element.get<CompFirst>().x);
+            ASSERT_EQ(2, element.get<CompSecond>().r);
+        }
+    }
 }
 
 TEST(cmd_buffer_test, world_test) {
@@ -93,20 +303,39 @@ TEST(cmd_buffer_test, world_test) {
 
     std::vector<Entity> entities;
     
-    for (int i = 0; i < 15; i++) {
+    for (int i = 0; i < 20; i++) {
         Entity e = world.newEntity();
         entities.push_back(e);
     }
 
+    // Test entity delete
     cmd_buffer.deleteEntity(entities.at(0));
     world.executeCmdBuffer(cmd_buffer);
-
     ASSERT_EQ(0, world.newEntity().id());
 
     // Attach components test
-    for (int i = 0; i < 15; i++) {
-        cmd_buffer.attachComponent<CompFirst>(entities.at(i));
+    for (int i = 0; i < 10; i++) {
+        cmd_buffer.attachComponent<CompFirst>(entities.at(i), i);
     }
 
     world.executeCmdBuffer(cmd_buffer);
+
+    // Detach components
+    {
+        auto query = world.createQuery<CompFirst>();
+        
+        ASSERT_EQ(10, query.size());
+        for (auto element : query) {
+            if (element.get<CompFirst>().x < 5) {
+                cmd_buffer.detachComponent<CompFirst>(element.entity());
+            }
+        }
+    }
+
+    world.executeCmdBuffer(cmd_buffer);
+    
+    {
+        auto query = world.createQuery<CompFirst>();
+        ASSERT_EQ(5, query.size());
+    }
 }
