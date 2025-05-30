ca8243ebf885cfb753610a4b6aebc25aa513a6f7
Andrea Greco <113620544+mrMart1an@users.noreply.github.com>
2025-05-30 22:24:28 +0200

    bevy credit

--- include/conduit/ecs/world.h
+++ include/conduit/ecs/world.h
@@ -1,6 +1,9 @@
 #ifndef CNDT_ECS_WORLD_H
 #define CNDT_ECS_WORLD_H
 
+// Credits to the Bevy game engine for inspiring this ECS implementation
+// https://github.com/bevyengine/bevy
+
 #include "conduit/ecs/entity.h"
 
 #include "conduit/internal/ecs/entityRegister.h"
